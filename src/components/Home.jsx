110380934f67f3e99ec0dc3110df2efdd3d42306
Yeshwanth Sai Gokarakonda <y.gokarakonda@accenture.com>
2025-10-30 15:33:18 +0530

    centered contents to the page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { upload } from '../assets';
 function Home() {
     return (
         <>
-        <div className="container">
+        <div className="container home-container">
             <div className="row main">
                 <div className="col-8 left-div">
                     <div className="avatar-div">
@@ -18,14 +18,16 @@ function Home() {
                                 <p>Just say AKIRA and let me know the report you are looking for</p>
                             </div>
                             <div className="mic-controls">
-                                <img src={mic} alt="" />
-                                <img src={upload} alt="" />
+                                <img src={mic} alt="mic" />
+                                <img src={upload} alt="upload" />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-4 right-div">
-                    Hi
+                    <div className="cards">
+                        <p>Ask AKIRA</p>
+                    </div>
                 </div>
             </div>
         </div>
