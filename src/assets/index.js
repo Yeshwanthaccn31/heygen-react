1a38e652d72425092cb4c320da925aa883b45b62
Yeshwanth Sai Gokarakonda <y.gokarakonda@accenture.com>
2025-10-30 13:02:42 +0530

    basic homepage ui

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg'
 import menu from './Menu.svg'
+import avatar from './avatar.png'
+import mic from './mic.svg'
+import upload from './upload.svg'
 
 export {
   logo,
   menu,
+  avatar, 
+  mic, 
+  upload
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,30 @@
 import '../styles/home.css';
+import avatar from '../assets/avatar.png';
+import { mic } from '../assets';
+import { upload } from '../assets';
 
 function Home() {
     return (
         <>
         <div className="container">
-            <div className="row">
-                <div className="col-8">
+            <div className="row main">
+                <div className="col-8 left-div">
                     <div className="avatar-div">
                         <div className="avatar">
-                            
+                            <img src={avatar} alt="" />
                         </div>
                         <div className="controls">
-                            
+                            <div className="welcome-message">
+                                <p>Just say AKIRA and let me know the report you are looking for</p>
+                            </div>
+                            <div className="mic-controls">
+                                <img src={mic} alt="" />
+                                <img src={upload} alt="" />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 right-div">
                     Hi
                 </div>
             </div>
